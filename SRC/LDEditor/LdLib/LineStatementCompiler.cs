ca9208dc92de301798b2ffd4bbf041f1db6900d8
Hailey <hailey@not_an_email.fake>
2024-02-19 06:20:49 -0600

    worss before its better

--- SRC/LDEditor/LdLib/LineStatementCompiler.cs
+++ SRC/LDEditor/LdLib/LineStatementCompiler.cs
@@ -13,22 +13,17 @@ public enum Operator
 public class Node
 {
     public Operator Operator { get; set; }
-    public Node Left { get; set; }
-    public Node Right { get; set; }
-    public LdElement Element { get; set; }
+    public Node? Left { get; set; }
+    public Node? Right { get; set; }
+    public LdElement? Element { get; set; }
 
 
     public override string ToString()
     {
         if (Element != null) return Element.Label;
-        var left = Left?.ToString();
-        var right = Right?.ToString();
-        return Operator switch
-        {
-            Operator.And => $"({left} AND {right})",
-            Operator.Or => $"({left} OR {right})",
-            _ => throw new System.Exception()
-        };
+        var left = Left?.ToString() ?? "";
+        var right = Right?.ToString() ?? "";
+        return $"({left} {Operator} {right})";
     }
 }
 
@@ -76,6 +71,7 @@ public class LineStatementCompiler
     public override string ToString()
     {
         var root = Parse(0, 0);
+
         return root.ToString();
     }
 
@@ -91,24 +87,18 @@ public class LineStatementCompiler
             case ElementType.Wire: return Parse(r, c + 1);
             case ElementType.NormallyOpenContact:
             case ElementType.NormallyClosedContact:
-                var next = GetElement(c + 1, r);
-                if (next != null || c >= _maxCols - 1)
-                {
-                    return new Node()
-                    {
-                        Operator = Operator.And,
-                        Left = new Node()
-                        {
-                            Element = elem
-                        },
-                        Right = Parse(r, c + 1)
-                    };
-                }
 
                 return new Node()
                 {
-                    Element = elem
+                    Operator = Operator.And,
+                    Left = new Node()
+                    {
+                        Element = elem
+                    },
+                    Right = Parse(r, c + 1)
                 };
+
+
             default: throw new Exception();
         }
     }
