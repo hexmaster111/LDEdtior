2f4da2f807a4f34597043b01a48fb66e03d0181d
Hailey <Hailey@NotAnEmail.com>
2024-07-27 16:30:09 -0500

    drawing on the grid

--- LdDiagramGenerator/LdDiagramGenerator/LdGraphicalDiagram/Program.cs
+++ LdDiagramGenerator/LdDiagramGenerator/LdGraphicalDiagram/Program.cs
@@ -4,7 +4,6 @@ using static Raylib_CsLo.Raylib;
 using static Raylib_CsLo.RayGui;
 using static Raylib_CsLo.RayMath;
 using static Raylib_CsLo.RlGl;
-
 using System.Numerics;
 
 
@@ -90,18 +89,17 @@ InitWindow(screenWidth, screenHeight, "LD");
 Camera2D camera = new();
 camera.zoom = 1.0f;
 
-int zoomMode = 0;   // 0-Mouse Wheel, 1-Mouse Move
+int zoomMode = 0; // 0-Mouse Wheel, 1-Mouse Move
 
-var tilesTexture = LoadTexture("sc.png");
+var sprites = LoadTexture("Assets/sc.png");
+if (sprites.id == 0) throw new Exception("Could not load assets!");
+
 int inBetweenSpriteSpace = 1;
 int spriteSize = 64;
 
-
-SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-                                    //--------------------------------------------------------------------------------------
-
+SetTargetFPS(60);
 // Main game loop
-while (!WindowShouldClose())        // Detect window close button or ESC key
+while (!WindowShouldClose())
 {
     // Update
     //----------------------------------------------------------------------------------
@@ -109,7 +107,7 @@ while (!WindowShouldClose())        // Detect window close button or ESC key
     else if (IsKeyPressed(KeyboardKey.KEY_TWO)) zoomMode = 1;
 
     // Translate based on mouse right click
-    if (IsMouseButtonDown(MouseButton.MOUSE_BUTTON_RIGHT))
+    if (IsMouseButtonDown(MouseButton.MOUSE_BUTTON_RIGHT) || IsKeyDown(KeyboardKey.KEY_LEFT_ALT))
     {
         Vector2 delta = GetMouseDelta();
         delta = Vector2Scale(delta, -1.0f / camera.zoom);
@@ -144,45 +142,59 @@ while (!WindowShouldClose())        // Detect window close button or ESC key
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
 
     BeginMode2D(camera);
 
     // Draw the 3d grid, rotated 90 degrees and centered around 0,0 
     // just so we have something in the XY plane
     rlPushMatrix();
-    rlTranslatef(0, 25 * 50, 0);
+    rlTranslatef(0, 25 * 64, 0);
     rlRotatef(90, 1, 0, 0);
     DrawGrid(100, 64);
     rlPopMatrix();
 
 
     SetMouseOffset((int)camera.target.X, (int)camera.target.Y);
-    //SetMouseScale(1 + camera.zoom, 1 + camera.zoom);
     var mp = GetMousePosition();
     DrawCircle((int)mp.X, (int)mp.Y, 5, RED);
-    // if (GuiButton(new Rectangle(0, 0, 128, 24), "Some Button"))
-    // {
+    //GuiDrawIcon((int)GuiIconName.ICON_CURSOR_HAND, (int)mp.X, (int)mp.Y, 1, WHITE);
 
-    // }
+    for (var i = 1; i < 15; i++)
+    {
+        DrawSpriteOnGrid(i, 0, Sprite.DownWire);
+        DrawSpriteOnGrid(i, 10, Sprite.DownWire);
+    }
+
+    DrawSpriteOnGrid(1, 0, Sprite.BranchStart);
+    DrawSpriteOnGrid(1, 1, Sprite.Wire);
+
+    DrawLdSpriteOnGrid(1, 2, "X01", Sprite.No);
+
+    //DrawSpriteOnGrid(1, 2, Sprite.No);
+    DrawSpriteOnGrid(1, 3, Sprite.OrBranch);
+
+    DrawSpriteOnGrid(2, 3, Sprite.DownWire);
+
+    DrawSpriteOnGrid(3, 3, Sprite.OrBranchStart);
+    DrawLdSpriteOnGrid(3, 4, "X02", Sprite.No);
+    DrawSpriteOnGrid(3, 5, Sprite.OrBranchEnd);
+
+    DrawSpriteOnGrid(2, 5, Sprite.DownWire);
+
+    DrawLdSpriteOnGrid(1, 4, "X03", Sprite.No);
+    DrawSpriteOnGrid(1, 5, Sprite.OrBranch);
+    DrawSpriteOnGrid(1, 6, Sprite.Wire);
+    DrawSpriteOnGrid(1, 7, Sprite.Wire);
+    DrawSpriteOnGrid(1, 8, Sprite.Wire);
+    DrawLdSpriteOnGrid(1, 9, "X02", Sprite.Coil);
+    DrawSpriteOnGrid(1, 10, Sprite.BranchEnd);
 
 
-    const int gridWidthPx = 64, gridHeightPx = 64;
-
-    int gridRow = 1, gridCol = 1;
-
-    int tlPx = gridWidthPx * gridRow;
-    int tlPy = gridHeightPx * gridCol;
-
-
-    //SetMouseScale(1, 1);
     SetMouseOffset(0, 0);
-
     EndMode2D();
-
     if (GuiButton(new Rectangle(0, 0, 128, 24), "Some Button"))
     {
-
     }
 
     EndDrawing();
@@ -191,10 +203,49 @@ while (!WindowShouldClose())        // Detect window close button or ESC key
 
 // De-Initialization
 //--------------------------------------------------------------------------------------
-CloseWindow();        // Close window and OpenGL context
-                      //--------------------------------------------------------------------------------------
+CloseWindow(); // Close window and OpenGL context
+//--------------------------------------------------------------------------------------
+const int gridWidthPx = 64, gridHeightPx = 64, spritePaddingPx = 1, labelFontSize=32;
 return 0;
 
 
+void DrawLdSpriteOnGrid(int row, int col, string lbl, Sprite spriteidx)
+{
+    DrawTextOnGrid(row - 1, col, lbl);
+    DrawSpriteOnGrid(row, col, spriteidx);
+}
+
+void DrawTextOnGrid(int row, int col, string text)
+{
+    int tlPx = gridWidthPx * col;
+    int tlPy = gridHeightPx * row;
+
+    DrawText(text, tlPx, tlPy, labelFontSize, WHITE);
+}
+
+void DrawSpriteOnGrid(int row, int col, Sprite spriteIdx)
+{
+    int tlPx = gridWidthPx * col;
+    int tlPy = gridHeightPx * row;
 
 
+    var scLocation = new Rectangle((int)spriteIdx * 64 + (spritePaddingPx * (int)spriteIdx), 0, 64, 64);
+    var destLocation = new Rectangle(tlPx, tlPy, 64, 64);
+    DrawTexturePro(sprites, scLocation, destLocation, new(0, 0), 0, YELLOW);
+}
+
+enum Sprite
+{
+    Wire,
+    No,
+    Nc,
+    Coil,
+    CoilSet,
+    CoilReset,
+    OrBranch,
+    OrBranchStart,
+    OrBranchEnd,
+    DownWire,
+    BranchStart,
+    BranchEnd
+}
